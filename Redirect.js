15122bd307790dfe71bde5d828291a7bb36f4c28
PlasmaPuffs-Productions <PlasmaPuffsProductions@gmail.com>
2025-09-14 19:58:57 -0700

    Optimize redirection speed

--- Redirect.js
+++
@@ -1,6 +0,0 @@
-(() => {
-        if (!window.location.search.includes("redirect=")) {
-                const path = window.location.pathname + window.location.search + window.location.hash;
-                window.location.replace("/Index.html?redirect=" + encodeURIComponent(path));
-        }
-})();
